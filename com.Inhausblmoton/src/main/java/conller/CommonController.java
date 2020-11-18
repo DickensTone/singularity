dc10c0a250261ebb711f54b2a5615f2a7dbbed9d
ddt <83404639@qq.com>
2020-11-18 22:43:05 +0800

    init

---
+++ com.Inhausblmoton/src/main/java/conller/CommonController.java
@@ -0,0 +1,14 @@
+package main.java.conller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+
+	@GetMapping("*")
+	public String wxLogin() {
+		
+		return null;
+	}
+}
