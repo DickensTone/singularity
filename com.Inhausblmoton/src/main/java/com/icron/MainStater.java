26deb4512b1f75f3d9535a3cc4b6170643ac3eaa
dickenstone <834104639@qq.com>
2020-11-19 20:40:02 +0800

    first commit test git

---
+++ com.Inhausblmoton/src/main/java/com/icron/MainStater.java
@@ -0,0 +1,13 @@
+package com.icron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainStater {
+	
+	  public static void main(String[] args) {
+		    SpringApplication.run(MainStater.class, args);
+	  }
+
+}
---
+++ com.Inhausblmoton/src/main/java/com/icron/conller/CommonController.java
@@ -0,0 +1,14 @@
+package com.icron.conller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+
+	@GetMapping("*")
+	public String wxLogin() {
+		
+		return null;
+	}
+}
